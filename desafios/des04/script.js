0230f23f209aff73818ced8aa73b3f5680857dfa
CarlosEdson <carlosedsonduarte@gmail.com>
2024-07-02 19:00:52 -0300

    exercicio de JS

--- desafios/des04/script.js
+++ desafios/des04/script.js
@@ -1,5 +1,15 @@
+var msg = document.querySelector('#msg')
+var img = document.querySelector('#imagem')
+var data = new Date()
+
 function carregar(){
-    var msg = document.querySelector('#msg')
-    var img = document.querySelector('#imagem')
-    var data = new Date()
+    var hora = data.getHours()
+    msg.innerHTML = `Agora são ${hora} horas`
+    if(hora >= 0 && hora < 12){
+        img.src = 'imagens/nascente.jpg'
+    } else if (hora >= 13 && hora <= 18){
+        img.src = 'imagens/tarde.jpg'
+    } else{
+        img.src = 'imagens/noite.jpg'
+    }
 }
