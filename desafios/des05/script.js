a832485ac2ff101bb39337a837213773bb2319a3
CarlosEdson <carlosedsonduarte@gmail.com>
2024-07-03 15:07:45 -0300

    desafio JS

--- desafios/des05/script.js
+++ desafios/des05/script.js
@@ -1,15 +1,14 @@
 var cx1 = document.querySelector('#num')
 var res = document.querySelector('#res')
+var data = new Date() //data atual
+var ano = data.getFullYear() //pegar ano com 4 digitos
 
 function consultar(){
-    var ano_nascimento = Number(cx1.valeu)
-    var data = new Date() //data atual
-    var ano = data.getFullYear() //ano atual
-
-    var idade = ano_nascimento - ano
-    res.innerHTML = idade
-
-    if(ano_nascimento.valeu.length == 0 || ano_nascimento.value > ano){
-        window.alert('Erro verifique os dados')
+    if(cx1.value.length == 0 || cx1.value > ano){
+        window.alert('[ERRO] Verifique seus Dados')
+    } else{
+        var fsex = document.getElementsByTagName('sex')
+        var idade = ano - Number(cx1.value)
+        res.innerHTML = `Você tem ${idade}`
     }
 }
