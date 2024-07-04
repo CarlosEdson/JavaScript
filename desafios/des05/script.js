c70420411259354f88a9c3393e8e0a1af0dd10c5
CarlosEdson <carlosedsonduarte@gmail.com>
2024-07-04 10:19:24 -0300

    exercicio de js não esta dando o resultado correspondido

--- desafios/des05/script.js
+++ desafios/des05/script.js
@@ -1,14 +1,19 @@
-var cx1 = document.querySelector('#num')
-var res = document.querySelector('#res')
-var data = new Date() //data atual
-var ano = data.getFullYear() //pegar ano com 4 digitos
-
 function consultar(){
+    var cx1 = document.querySelector('#num')
+    var res = document.querySelector('#res')
+    var data = new Date() //data atual
+    var ano = data.getFullYear() //pega o ano com 4 digitos
     if(cx1.value.length == 0 || cx1.value > ano){
         window.alert('[ERRO] Verifique seus Dados')
     } else{
         var fsex = document.getElementsByTagName('sex')
         var idade = ano - Number(cx1.value)
-        res.innerHTML = `Você tem ${idade}`
+        var genero = ''
+        if(fsex[0].checked){
+            genero = 'Homem'
+        } else if(fsex[1].checked){
+            genero = 'Mulher'
+        }
+        res.innerHTML = `Detectamos um ${genero} com ${idade} anos`
     }
 }
