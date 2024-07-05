c6c4ccb214fef18c0ab310979ee9f6e416ddc49f
CarlosEdson <carlosedsonduarte@gmail.com>
2024-07-05 15:05:59 -0300

    teste para resolução de erro em compilação

--- desafios/des06/script.js
+++ desafios/des06/script.js
@@ -1,13 +1,14 @@
-var ano_nascimento = document.querySelector('#nascimento')
-var data = new Date()
-var ano = data.getFullYear()
-var res = document.querySelector('#res')
-
 function consultar(){
-    if(ano_nascimento.value.length == 0 || ano_nascimento > ano){
-        window.alert('Seus dados estão Incorretos')
-        var idade = ano - Number(nascimento.value)
-    }
+    var ano_nascimento = document.querySelector('#nascimento')
+    var data = new Date()
+    var ano = data.getFullYear()
+    var res = document.querySelector('#res')
 
-    res.innerHTML = `Você tem ${idade}`
+    if(ano_nascimento.value.length == 0 || ano_nascimento.value > ano){
+        window.alert('Seus dados estão Incorretos')
+    } else{
+        var idade = ano - Number(ano_nascimento.value)
+        var sexo = document.getElementsByClassName('radsex')
+        res.innerHTML = `Sua idade é ${idade}`
+    }
 }
