f8f94f7a1f0edf503a803d4242f2d02061d4f013
CarlosEdson <carlosedsonduarte@gmail.com>
2024-07-06 14:51:08 -0300

    resolução do erro de compilação

--- desafios/des06/script.js
+++ desafios/des06/script.js
@@ -8,13 +8,30 @@ function consultar(){
         window.alert('Seus dados estão Incorretos')
     } else {
         var idade = ano - Number(ano_nascimento.value)
-        var sexo = document.getElementsByClassName('radsex')
+        var sexo = document.getElementsByName('radsex')
         var genero = ''
         if(sexo[0].checked){
             genero = 'Homem'
+            if(idade <= 12){
+                res.innerHTML = `Você é uma criança de ${idade} anos`
+            } else if(idade <= 25){
+                res.innerHTML = `Você é um adolecente de ${idade} anos`
+            } else if(idade >= 26 && idade <= 55){
+                res.innerHTML = `Você é um Adulto de ${idade} anos`
+            } else if(idade > 56){
+                res.innerHTML = `Você é um Idoso de ${idade} anos`
+            }
         } else {
             genero = 'Mulher'
+            if(idade <= 12){
+                res.innerHTML = `Você é uma criança de ${idade} anos`
+            } else if(idade <= 25){
+                res.innerHTML = `Você é uma adolecente de ${idade} anos`
+            } else if(idade >= 26 && idade <= 55){
+                res.innerHTML = `Você é uma Mulher Adulta de ${idade} anos`
+            } else if(idade > 56){
+                res.innerHTML = `Você é uma Idosa de ${idade} anos`
+            }
         }
-        res.innerHTML = `Você é um ${genero} de ${idade} anos`
     }
 }
