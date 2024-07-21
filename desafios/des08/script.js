c57c4c95d243692a9b69823f89eab5b01a3ceb9e
CarlosEdson <carlosedsonduarte@gmail.com>
2024-07-20 22:25:09 -0300

    aperfeiçoando o meu JS

---
+++ desafios/des08/script.js
@@ -0,0 +1,17 @@
+function calcular(){
+    var inicio = document.querySelector('#num1')
+    var fim = document.querySelector('#num2')
+    var passo = document.querySelector('#passo')
+
+    if(inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0){
+        window.alert('ERRO. falta de dados')
+    } else{
+        let i = Number(inicio.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+        resultado.innerHTML = 'contando'
+        for(let c = i; c <= f; c += p){
+            resultado.innerHTML += `${c}`
+        }
+    }
+}
