674e1c2aba905d54ce6c57ec77ccb095baaaa2e2
CarlosEdson <carlosedsonduarte@gmail.com>
2024-07-30 22:48:11 -0300

    desafio de function e arrays

---
+++ desafios/des20/script.js
@@ -0,0 +1,10 @@
+let cx1 = document.querySelector('#num')
+function adicionar(){
+    let valor = Number(cx1.value)
+    
+    if(valor == ''){
+        window.alert('ERRO, o campo a baixo precisa ser preenchido')
+    } else{
+        resultado.innerHTML = `Você adicionou o valor ${valor}`
+    }
+}
