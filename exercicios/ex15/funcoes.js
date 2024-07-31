629f38c068f9b279c87f69ca58dec8a0787d6f41
CarlosEdson <carlosedsonduarte@gmail.com>
2024-07-30 22:29:23 -0300

    exercicios de funções

---
+++ exercicios/ex15/funcoes.js
@@ -0,0 +1,23 @@
+/*
+function parimpar(n){
+    if(n%2 == 0){
+        return 'par'
+    } else{
+        return 'impar'
+    }
+}
+let resultado = parimpar(4)
+console.log(resultado)
+*/
+/*
+function soma(n1,n2){
+    return n1 + n2
+}
+let res = soma(6,4)
+console.log(res)
+*/
+let valor = function(x){
+    return 5*x
+}
+let res = valor(8)
+console.log(res)
