2a5035653a9d45aee778269039de8bc408e39e37
CarlosEdson <carlosedsonduarte@gmail.com>
2024-08-02 21:24:05 -0300

    desafio do guanabara

--- desafios/des20/script.js
+++ desafios/des20/script.js
@@ -1,11 +1,31 @@
-let cx1 = document.querySelector('#num')
+let numeros = document.querySelector('#num')
 let valores = []
-function adicionar(){
-    let valor = Number(cx1.value)
-    
-    if(valor == ''){
-        window.alert('ERRO, o campo a baixo precisa ser preenchido')
+let compararnumeoros = document.querySelector('#conferenumeros')
+
+function isnumero(n){
+    if(Number(n) >= -1 && Number(n) <= 50){
+        return true
     } else{
-        resultado.innerHTML = `Você adicionou o valor ${valor}`
+        return false
     }
+}
+function inlista(n, l){
+    if(l.indexOf(Number(n)) != -1){
+        return true
+    } else{
+        return false
+    }
+}
+
+function adicionar(){
+    if(isnumero(numeros.value) && !inlista(numeros.value, valores)){
+        valores.push(Number(numeros.value))
+        let item = document.createElement('option')
+        item.text = `Valor ${numeros.value} adicionado`
+        compararnumeoros.appendChild(item)
+    }else{
+        window.alert('ERRO, o valor já esta presente em lista')
+    }
+    numeros.value = ''
+    numeros.focus()
 }
