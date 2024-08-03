b931bc4ba6a9021da30c81ca0cd4a6a2613f3078
CarlosEdson <carlosedsonduarte@gmail.com>
2024-08-03 15:33:35 -0300

    fim do desafio do guanabara

--- desafios/des20/script.js
+++ desafios/des20/script.js
@@ -23,9 +23,34 @@ function adicionar(){
         let item = document.createElement('option')
         item.text = `Valor ${numeros.value} adicionado`
         compararnumeoros.appendChild(item)
+        resultado.innerHTML = ''
     }else{
         window.alert('ERRO, o valor já esta presente em lista')
     }
     numeros.value = ''
     numeros.focus()
+}
+
+function finalizar(){
+    if(valores.length == 0){
+        window.alert('Adicione valores')
+    } else{
+        let total = valores.length
+        let maior = valores[0]
+        let menor = valores[0]
+        let soma = 0
+
+        for(let pos in valores){
+            soma += valores[pos]
+            if(valores[pos] > maior ){
+                maior = valores[pos]
+            }
+            if(valores[pos] < menor){
+                menor = valores[pos]
+            }
+        }
+        resultado.innerHTML = ''
+        resultado.innerHTML += `Você tem ${total} elementos <br>`
+        resultado.innerHTML += `O seu maior número é ${maior} e o meinor é ${menor} <br>`
+    }
 }
